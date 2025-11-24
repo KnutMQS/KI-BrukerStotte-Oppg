03d344b1cc806b3cf3f9f5b83c43ad6de34d8c08
KnutMQS <knutmqs@gmail.com>
2025-11-24 09:44:23 +0100

    fix: update Pros component for consistency and styling

--- ki-kurs/src/pages/pros.jsx
+++ ki-kurs/src/pages/pros.jsx
@@ -5,40 +5,40 @@ function Pros() {
         <h1>Fordeler med KI</h1>
       </div>
       <div className="space-y-7">
-        <ul className="max-w-fit mx-auto border rounded-4xl border-indigo-600 p-4">
+        <ul className="max-w-fit mx-auto border rounded-4xl bg-indigo-950 border-indigo-600 p-4">
           <li className="space-y-3">
             <h3 className="flex justify-center items-center text-2xl underline">
               Automasjon og effekivitet:
             </h3>
             <span>
-              KI kan automatisere repetitive oppgaver og øke produktiviteten
+              KI kan automatisere repetitive oppgaver og øke produktiviteten.
             </span>
           </li>
         </ul>
-        <ul className="max-w-fit mx-auto border rounded-4xl border-indigo-600 p-4">
+        <ul className="max-w-fit mx-auto border rounded-4xl bg-indigo-950 border-indigo-600 p-4">
           <li className="space-y-3">
             <h3 className="flex justify-center items-center text-2xl underline">
               Redusert Menneskelige Feil:
             </h3>
             <span>
-              Konsistente og nøyaktige resultater i definerte oppgaver
+              Konsistente og nøyaktige resultater i definerte oppgaver.
             </span>
           </li>
         </ul>
-        <ul className="max-w-fit mx-auto border rounded-4xl border-indigo-600 p-4">
+        <ul className="max-w-fit mx-auto border rounded-4xl bg-indigo-950 border-indigo-600 p-4">
           <li className="space-y-3">
             <h3 className="flex justify-center items-center text-2xl underline">
               Tilgjengelig 24/7:
             </h3>
-            <span>KI systemer kan jobber kontinuerlig uten pauser</span>
+            <span>KI systemer kan jobber kontinuerlig uten pauser.</span>
           </li>
         </ul>
-        <ul className="max-w-fit mx-auto border rounded-4xl border-indigo-600 p-4">
+        <ul className="max-w-fit mx-auto border rounded-4xl bg-indigo-950 border-indigo-600 p-4">
           <li className="space-y-3">
             <h3 className="flex justify-center items-center text-2xl underline">
               Data Analyse:
             </h3>
-            <span>Behandle og analysere store datasett raskt</span>
+            <span>Behandle og analysere store datasett raskt.</span>
           </li>
         </ul>
       </div>
